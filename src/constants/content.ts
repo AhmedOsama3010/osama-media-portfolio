39a9d5f9fd72d7fde281f4d379fc1f68db413ec3
AhmedOsama3010 <rsha12606@gmail.com>
2025-07-23 21:13:52 +0300

    Update content.ts
    اضافة الريلز

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -217,7 +217,21 @@ export const content = {
         videoUrl: "https://player.vimeo.com/video/1091499879?h=205bf13ad9"
       },
       //========================{ريلز}========================= 
-      {
+    {
+        id: 9,
+        title: {
+          ar: "",
+          en: ""
+        },
+        description: {
+          ar: "مونتاج ريلز قصير مع تأثيرات صوتية ومرئية جذابة",
+          en: ""
+        },
+        category: "reels",
+        videoUrl:  "https://www.youtube.com/embed/ymGiRXskn2o",
+        isNew: true 
+      },
+    {
         id: 9,
         title: {
           ar: "",
