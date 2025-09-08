cee94f85bdc8f0486b9d6ea30d38fbf0844c622a
AhmedOsama3010 <rsha12606@gmail.com>
2025-09-08 19:49:33 +0300

    Update content.ts

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -242,7 +242,7 @@ export const content = {
           en: ""
         },
         category: "reels",
-        videoUrl:  "https://player.vimeo.com/video/1116830268?share=copy",
+        videoUrl:  "https://player.vimeo.com/video/1116829906",
         isNew: true 
       },
     {
