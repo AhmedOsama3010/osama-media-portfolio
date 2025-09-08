415ff753467a35e8fadc538ea5a9ca30225e3c2f
AhmedOsama3010 <rsha12606@gmail.com>
2025-09-08 19:21:12 +0300

    Update content.ts

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -217,6 +217,20 @@ export const content = {
         videoUrl: "https://player.vimeo.com/video/1091499879?h=205bf13ad9"
       },
       //========================{ريلز}========================= 
+{
+        id: 9,
+        title: {
+          ar: "",
+          en: ""
+        },
+        description: {
+          ar: "مونتاج ريلز قصير مع تأثيرات صوتية ومرئية جذابة",
+          en: ""
+        },
+        category: "reels",
+        videoUrl:  "https://player.vimeo.com/video/1116829906?h",
+        isNew: true 
+      },
     {
         id: 9,
         title: {
